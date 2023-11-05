525f309425cc4a956af984b473099cc108fd036c
Vinicius CZB <viniczb@gmail.com>
2023-11-05 15:21:27 +0100

    Create initial UI

---
+++ src/ui/Form.tsx
@@ -0,0 +1,35 @@
+import { FormEvent } from "react";
+
+import "./Form.css";
+
+const CATEGORIES = ["Mercado", "Lazer", "Casa"];
+
+const Form = () => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+
+    const category = data.get("category") as string;
+    const price = parseFloat(data.get("price") as string);
+    const description = (data?.get("description") || "") as string;
+
+    console.log(category, price, description);
+  };
+
+  return (
+    <form id="form" onSubmit={(e) => handleSubmit(e)}>
+      <select required name="category" placeholder="Categoria">
+        {CATEGORIES.map((category) => (
+          <option key={category}>{category}</option>
+        ))}
+      </select>
+
+      <input placeholder="Valor" type="number" name="price" required />
+      <textarea placeholder="Descrição" rows={8} name="description" />
+      <button type="submit">Adicionar</button>
+    </form>
+  );
+};
+
+export default Form;
