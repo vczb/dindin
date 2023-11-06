443eb8b364932ed05984dd9623868f72c8145d31
Vinicius CZB <viniczb@gmail.com>
2023-11-06 21:31:12 +0100

    Use Sagu-UI components

--- src/ui/Form.tsx
+++ src/ui/Form.tsx
@@ -2,10 +2,10 @@ import { FormEvent } from "react";
 
 import { insertItem } from "../services/firebase";
 
-import { Button, TextField } from "sagu-ui";
-import TextArea from "./TextArea";
+import { Button, TextField, TextArea, Select } from "sagu-ui";
 import styled from "styled-components";
-import Select from "./Select";
+
+const OPTIONS = ["Mercado", "Lazer", "Casa"];
 
 const S = {
   Form: styled("form")`
@@ -24,7 +24,7 @@ const Form = () => {
     const category = data.get("category") as string;
     const price = parseFloat(data.get("price") as string);
     const description = (data?.get("description") || "") as string;
-
+    console.log({ category, price, description });
     insertItem({ category, price, description })
       .then(() => {
         console.log("success");
@@ -36,10 +36,10 @@ const Form = () => {
 
   return (
     <S.Form id="form" onSubmit={(e) => handleSubmit(e)}>
-      <Select />
-      <TextField type="number" name="price" required label="Valor" />
+      <Select options={OPTIONS} name="category" label="Categoria" />
+      <TextField name="price" type="number" required label="Valor" />
 
-      <TextArea />
+      <TextArea label="Descrição" name="description" />
 
       <Button type="submit" variant="filled">
         Adicionar
