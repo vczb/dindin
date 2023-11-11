1f16264870ab74d5d53a1d30b95d9e8699e3f85e
Vinicius CZB <viniczb@gmail.com>
2023-11-11 10:53:03 +0100

    Refactor for using custom hook

---
+++ src/hooks/useInventory.ts
@@ -0,0 +1,57 @@
+import { FormEvent, useCallback, useState } from "react";
+import { insertItem, insertItemProps } from "../services/firebase";
+import { TOAST_MESSAGE_EVENT, dispatch } from "../services/events";
+
+const FORM_STATUS = {
+  success: {
+    message: "Adicionado com sucesso!",
+    type: "success",
+  },
+  error: {
+    message: "Ops... algo deu errado",
+    type: "error",
+  },
+};
+
+const useInventory = () => {
+  const [isFormLoading, setIsFormLoading] = useState(false);
+
+  const getFormData = (event: FormEvent<HTMLFormElement>) => {
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const dataObject = Object.fromEntries(data.entries()) as any;
+
+    return dataObject;
+  };
+
+  const insertOnInventory = async (data: insertItemProps) => {
+    return await insertItem(data);
+  };
+
+  const handleSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setIsFormLoading(true);
+
+    const data = getFormData(event);
+
+    insertOnInventory(data)
+      .then(() => {
+        dispatch(TOAST_MESSAGE_EVENT, FORM_STATUS.success);
+      })
+      .catch(() => {
+        dispatch(TOAST_MESSAGE_EVENT, FORM_STATUS.error);
+      })
+      .finally(() => {
+        setIsFormLoading(false);
+      });
+  }, []);
+
+  return {
+    getFormData,
+    insertOnInventory,
+    handleSubmit,
+    isFormLoading,
+  };
+};
+export default useInventory;
--- src/ui/Form.tsx
+++ src/ui/Form.tsx
@@ -1,16 +1,6 @@
-import { FormEvent, useState } from "react";
-
-import { insertItem } from "../services/firebase";
-
-import {
-  Button,
-  TextField,
-  TextArea,
-  Select,
-  Toaster,
-  GenericInput,
-} from "sagu-ui";
+import { Button, Input, TextArea, Select } from "sagu-ui";
 import styled from "styled-components";
+import useInventory from "../hooks/useInventory";
 
 const OPTIONS = ["Mercado", "Restaurantes", "Passeios", "Casa", "Outros"];
 
@@ -22,65 +12,23 @@ const S = {
   `,
 };
 
-type StatusStateProps = {
-  message: string;
-  type: "error" | "success";
-};
-
 const Form = () => {
-  const [status, setStatus] = useState<StatusStateProps | null>(null);
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setStatus(null);
-
-    const data = new FormData(event.currentTarget);
-
-    const category = data.get("category") as string;
-    const price = parseFloat(data.get("price") as string);
-    const description = (data?.get("description") || "") as string;
-    const date = (data?.get("date") || "") as string;
-
-    console.log({ category, price, description });
-    insertItem({ category, price, description, date })
-      .then(() => {
-        setStatus({
-          message: "Adicionado com sucesso!",
-          type: "success",
-        });
-      })
-      .catch((error) => {
-        setStatus({
-          message: "Ops... algo deu errado",
-          type: "error",
-        });
-        console.log(error);
-      });
-  };
+  const { handleSubmit, isFormLoading } = useInventory();
 
   return (
     <S.Form id="form" onSubmit={(e) => handleSubmit(e)}>
       <Select options={OPTIONS} name="category" label="Categoria" />
-      <TextField name="price" type="number" required label="Valor" step="0.1" />
-      <GenericInput
+      <Input name="price" type="number" required label="Valor" step="0.1" />
+      <Input
+        label="Data"
         type="date"
         name="date"
         defaultValue={new Date().toISOString().substring(0, 10)}
       />
       <TextArea label="Descrição" name="description" />
-      <Button type="submit" variant="filled">
-        Adicionar
+      <Button type="submit" variant="filled" disabled={isFormLoading}>
+        {isFormLoading ? "Carregando..." : "Adicionar"}
       </Button>
-      {!!status?.message && (
-        <Toaster
-          severity={status.type}
-          isFullWidth
-          duration={8000}
-          closable={false}
-        >
-          {status.message}
-        </Toaster>
-      )}
     </S.Form>
   );
 };
