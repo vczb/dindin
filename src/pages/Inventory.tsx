efb87ef28df2d167a2b6a1e6eae9fce6a4fc0eb2
Vinicius CZB <viniczb@gmail.com>
2023-11-15 22:01:29 +0100

    Add Breadcrumb to Inventory page

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -1,7 +1,17 @@
+import { Box, Breadcrumb } from "sagu-ui";
 import Form from "../ui/Form";
 
 const Inventory = () => {
-  return <Form />;
+  const breadcrumb = [
+    { name: "Inicio", link: "/" },
+    { name: "Adicionar", link: "" },
+  ];
+  return (
+    <Box flex="column">
+      <Breadcrumb items={breadcrumb} />
+      <Form />
+    </Box>
+  );
 };
 
 export default Inventory;
